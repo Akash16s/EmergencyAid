2bc3c9460c04aa3a198d4cc778d3b5d0e9c49315
Akash16s <akashs1698@gmail.com>
2018-09-13 16:37:35 +0530

    Maps view added to see ambulance status

---
+++ EmergencyHelperApp/app/src/main/java/com/akash/sky/emergencyhelper/MapsActivity.java
@@ -0,0 +1,68 @@
+package com.akash.sky.emergencyhelper;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    LocationManager locationManager;
+    static final int REQUEST_LOCATION=1;
+    private GoogleMap mMap;
+    double latti,longi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng user = new LatLng(latti, longi);
+        mMap.addMarker(new MarkerOptions().position(user).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user,15));
+    }
+
+
+    void getLocation(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String []{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }else{
+            Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(location !=null){
+                latti=location.getLatitude();
+                longi=location.getLongitude();
+            }
+        }
+    }
+
+}
--- EmergencyHelperApp/app/src/main/java/com/akash/sky/emergencyhelper/callAmb.java
+++ EmergencyHelperApp/app/src/main/java/com/akash/sky/emergencyhelper/callAmb.java
@@ -3,6 +3,7 @@ package com.akash.sky.emergencyhelper;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,6 +12,8 @@ import android.os.Bundle;
 import android.support.annotation.RequiresPermission;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,6 +22,7 @@ public class callAmb extends Activity {
     static final int REQUEST_LOCATION=1;
     LocationManager locationManager;
     double latti,longi;
+    Button map;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Management/Emergency");
     @Override
@@ -29,6 +33,7 @@ public class callAmb extends Activity {
         SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
         String restoredText = prefs.getString("name", null);     //gets the value
         //Log.d("new",restoredText);
+        map =(Button) findViewById(R.id.maps);
 
         locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
         getLocation();
@@ -36,9 +41,6 @@ public class callAmb extends Activity {
         myRef.child("user1/Location/Latitude").setValue(latti,"new");
         myRef.child("user1/Location/Longitude").setValue(longi,"new");
         myRef.child("user1/Location/Name").setValue(restoredText,"new");
-
-
-
 }
 void getLocation(){
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
@@ -50,5 +52,10 @@ void getLocation(){
                 longi=location.getLongitude();
             }
         }
+    }
+
+    public void callMap(View view){
+        Intent intent = new Intent(callAmb.this, MapsActivity.class);
+        startActivity(intent);
     }
 }
